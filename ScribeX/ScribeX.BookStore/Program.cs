ebf75dfc5997053d89186deeffd88ee850f19432
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-07-14 08:56:59 +0530

    http request pipeline

--- ScribeX/ScribeX.BookStore/Program.cs
+++ ScribeX/ScribeX.BookStore/Program.cs
@@ -5,7 +5,23 @@ namespace ScribeX.BookStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //add services to DI container
+
             var app = builder.Build();
+
+            //add http pipelines
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("request from first middleware");
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("request from second middleware");
+                await next();
+            });
 
             app.MapGet("/", () => "Hello World!");
 
