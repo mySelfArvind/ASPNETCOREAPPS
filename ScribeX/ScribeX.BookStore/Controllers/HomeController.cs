0c4d3d21240ec94d61d81f117157731bec9a5d8c
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-07-19 23:00:41 +0530

    controller concept and added AddControllersWithViews() in DI container

---
+++ ScribeX/ScribeX.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScribeX.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Index Action Method";
+        }
+    }
+}
--- ScribeX/ScribeX.BookStore/Startup.cs
+++ ScribeX/ScribeX.BookStore/Startup.cs
@@ -4,39 +4,16 @@
     {
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllersWithViews();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Request From First Middleware\n");
-                await next();
-                await context.Response.WriteAsync("Response From First Middleware\n");
-            });
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Request From Second Middleware\n");
-                await next();
-                await context.Response.WriteAsync("Response From Second Middleware\n");
-            });
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Request From Third Middleware\n");
-                await next();
-                await context.Response.WriteAsync("Response From Third Middleware\n");
-            });
-
             app.UseEndpoints(endPoints =>
             {
-                endPoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello from App\n");
-                });
+                endPoints.MapDefaultControllerRoute();
             });
 
         }
