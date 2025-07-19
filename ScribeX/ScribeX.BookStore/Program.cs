ccf5881d672521b628e23b01d16194cc75bba5e5
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-07-19 22:38:49 +0530

    added Startup class and defined some request pipelines method

--- ScribeX/ScribeX.BookStore/Program.cs
+++ ScribeX/ScribeX.BookStore/Program.cs
@@ -4,28 +4,15 @@ namespace ScribeX.BookStore
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            CreateBuilder(args).Build().Run();
+        }
 
-            //add services to DI container
-
-            var app = builder.Build();
-
-            //add http pipelines
-            app.Use(async (context, next) =>
+        public static IHostBuilder CreateBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
             {
-                await context.Response.WriteAsync("request from first middleware");
-                await next();
+                webBuilder.UseStartup<Startup>();
             });
-
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("request from second middleware");
-                await next();
-            });
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
         }
     }
 }
---
+++ ScribeX/ScribeX.BookStore/Startup.cs
@@ -0,0 +1,44 @@
+﻿namespace ScribeX.BookStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Request From First Middleware\n");
+                await next();
+                await context.Response.WriteAsync("Response From First Middleware\n");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Request From Second Middleware\n");
+                await next();
+                await context.Response.WriteAsync("Response From Second Middleware\n");
+            });
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Request From Third Middleware\n");
+                await next();
+                await context.Response.WriteAsync("Response From Third Middleware\n");
+            });
+
+            app.UseEndpoints(endPoints =>
+            {
+                endPoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello from App\n");
+                });
+            });
+
+        }
+    }
+}
