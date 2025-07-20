bd72e1587b5a7f9b44afa762a132391a1a71837b
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-07-20 13:56:46 +0530

    Added new controller

---
+++ ScribeX/ScribeX.BookStore/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ScribeX.BookStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public string AllEmployees()
+        {
+            return "All Employee";
+        }
+
+        public string EmployeeId(int id)
+        {
+            return $"Employee with Id: {id}";
+        }
+
+        public string EmployeeByNameAndDepartment(string name, string department)
+        {
+            return $"Employee Name : {name} & Department : {department}";
+        }
+    }
+}
