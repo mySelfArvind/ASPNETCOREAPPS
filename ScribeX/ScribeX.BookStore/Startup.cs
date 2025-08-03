0b19dcbbf3c9b3b04003aefc11add79945184e96
Arvind Vishwakarma <arvindvishwakarma1010@gmail.com>
2025-08-03 12:54:30 +0530

    concept of static files and custom static folder and middleware for the same.

--- ScribeX/ScribeX.BookStore/Startup.cs
+++ ScribeX/ScribeX.BookStore/Startup.cs
@@ -1,4 +1,6 @@
-﻿namespace ScribeX.BookStore
+﻿using Microsoft.Extensions.FileProviders;
+
+namespace ScribeX.BookStore
 {
     public class Startup
     {
@@ -9,8 +11,13 @@
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyFiles")),
+                RequestPath = "/MyFiles"
+            });
             app.UseRouting();
-
             app.UseEndpoints(endPoints =>
             {
                 endPoints.MapDefaultControllerRoute();
